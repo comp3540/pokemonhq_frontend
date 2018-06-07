f9446e9d973464ec4e931d65b349a4794d580968
Tommy Baloukas <a_balou@live.concordia.ca>
2018-06-07 10:02:06 -0400

    Made changes to front end pages

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
-
 <template>
-
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Homepage></Homepage>
   </div>
-
 </template>
 
 <script>
-import Homepage from './components/Homepage.vue'
+  import Homepage from './components/Homepage.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Homepage
+  export default {
+    name: 'app',
+    components: {
+      Homepage
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,23 +1,15 @@
 <template>
-  <div class="">
-    
-    <body>
-        
-        <h1>POKEMON GO BACK!</h1>
-        <div id="loginInfo">
-            <a href="login.html"><h2>LOGIN</h2></a>
-            <a href="register.html"><h3>REGISTER</h3></a>
-        </div>
-
-    </body>
-</html>
- 
+  <div class="">  
+    <h1>POKEMON GO BACK!</h1>
+    <div id="loginInfo">
+        <a href="login.html"><h2>LOGIN</h2></a>
+        <a href="register.html"><h3>REGISTER</h3></a>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
- 
+  export default {
+   
   }
-}
 </script>
