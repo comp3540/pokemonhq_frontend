9e4a795f06b4c0a47b35b0f67406bc2e4809fa87
Rose Dufresne <rose.dufresne@outlook.com>
2018-07-03 17:30:44 -0400

    Rose lost permissions on files. Put them back

--- client/src/app/gameboard/components/ActiveCard.vue
+++ client/src/app/gameboard/components/ActiveCard.vue
@@ -2,16 +2,17 @@
 
   <div class="kard">
 
-    <div class="row-1">
+    <!-- for active card -->
+    <div class="row-1a" v-if="card.props.clazz === 'POKEMON'">
         <div class="stage"> {{card.props.stage}} </div>
         <div class="hp"> {{card.props.initialHP}} </div>
     </div>
 
-    <div class="row-2">
+    <div class="row-2a" v-if="card.props.clazz === 'POKEMON'">
         <div> {{card.props.name}} </div>
     </div>
 
-    <div class="row-3" v-if="card.props.abilities" v-for="ability in card.props.abilities" :key="ability.id">
+    <div class="row-3a" v-if="card.props.abilities" v-for="ability in card.props.abilities" :key="ability.id">
         <div class="ability-name"> {{ability.name}} </div>
         <div class="energy-req" v-for="energyReq in ability.energyReq" :key="energyReq.id">
             {{energyReq.type}}
@@ -20,10 +21,23 @@
         <div class="ability-desc"> {{ability.description}} </div>
     </div>
 
-    <div class="row-4">
+    <div class="row-4a" v-if="card.props.clazz === 'POKEMON'">
         <div class="retreat"> {{card.props.retreatCost}} </div>
         <div class="status"> {{card.props.status}} </div>
 
+    </div>
+
+    <!-- for trainer card -->
+    <div class="row-1b" v-if="card.props.clazz === 'TRAINER'">
+        <div class="category"> {{card.props.category}} </div>
+    </div>
+
+    <div class="row-2b" v-if="card.props.clazz === 'TRAINER'">
+        <div> {{card.props.name}} </div>
+    </div>
+
+    <div class="row-3b" v-if="card.props.clazz === 'TRAINER'">
+        <div class="ability"> {{card.props.ability}} </div>
     </div>
 
   </div>
@@ -57,7 +71,7 @@ export default {
         line-height: 35px;
     }
 
-    .row-1 {
+    .row-1a {
         display: flex;
         justify-content: space-between;
         height: 10%;
@@ -85,12 +99,12 @@ export default {
         font-size: 20px;
     }
 
-    .row-2 {
+    .row-2a {
         height: 10%;
         justify-content: center;
     }
 
-    .row-3 {
+    .row-3a {
         display: flex;
         flex-direction: row;
         justify-content: flex-start;
@@ -121,7 +135,7 @@ export default {
         text-align: left;
     }
 
-    .row-4 {
+    .row-4a {
         display: flex;
         height: 10%;
         justify-content: space-between;
@@ -139,6 +153,50 @@ export default {
         width: 30%;
         font-weight: bold;
         font-size: 14px;
+    }
+
+    .row-1b {
+        display: flex;
+        justify-content: space-between;
+        height: 15%;
+        border-radius: 10px 10px 0px 0px;
+        background-color: #cacaca;
+        flex-direction: row;
+    }
+
+    .row-2b {
+        background-color: #4f61b3;
+        height: 15%;
+        justify-content: center;
+        font-size: 18px;
+    }
+
+    .row-3b {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        height: 70%;
+        border-radius: 0px 0px 10px 10px;
+        background-color: #cacaca;
+        align-content: flex-start;
+        font-family: Helvetica;
+        flex-wrap: wrap;
+        line-height: 22px;
+        text-align: left;
+    }
+
+    .category {
+        width: 50%;
+        border-radius: 10px 0px 0px 0px;
+        font-size: 14px;
+        font-family: monospace;
+        background-color: rgb(151, 0, 0);
+        color: white;
+        font-weight: bold;
+    }
+
+    .ability {
+        font-size: 10px;
     }
 
 </style>
