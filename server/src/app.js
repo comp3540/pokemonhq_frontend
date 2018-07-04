6823ddfc2202e296778637b417e3f1e34bd9ee25
Tommy Baloukas <a_balou@live.concordia.ca>
2018-07-04 10:33:28 -0400

    Vue.js build files are now served from node.js

--- server/src/app.js
+++ server/src/app.js
@@ -5,13 +5,16 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const {sequelize} = require('./models');
 const app = express();
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
 dotenv.config();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(history());
+app.use(express.static(path.join(__dirname, './../../client/dist')));
 require('./routes')(app);
 require('./passport');
 sequelize.sync().then(() => {
