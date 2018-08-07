7452c1e226f4277377af62339bba8f9129434a0f
Liz Schraffenberger <liz.schraffenberger@gmail.com>
2018-08-06 20:25:47 -0400

    added unit test for Index.js. Removed unnecesary comments from ValidationError.test.js.

--- client/test/unit/Input.test.js
+++ client/test/unit/Input.test.js
@@ -1,20 +1,6 @@
-import {set, clear} from '../../src/utilities/errors/ValidationError.js';
+//Todo:
 
-test('set function', () => {
-	var fields = {field: 'cats'};
-	var errors = {};
-	function set (fields, errors){
-		expect(fields[field]).not.toEqual(errors[field]);
-	}
-	var errors = {field: 'kitter'};
-	function set (fields, errors){
-		expect(fields[field]).toEqual(errors[field]);
-	}
-	var fields = {};
-	function set (fields, errors){
-		expect(fields[field]).not.toEqual(errors[field]);
-	}
-})
+import {clear, getValue} from '../../src/utilities/form/Input.js';
 
 //For clear (fields, fieldName = undefined):
 // expect "fields" object's "error" property to be set to empty string IF "fields" object has a property "field"
@@ -34,4 +20,25 @@ test('clear function', () => {
 	function clear (testFields, fieldName = 'cade'){
 		expect(testFields[fieldName]).tobe('');
 	}
+})
+
+
+test('getValue function', () =>{
+	var fields = {field: 'cats'};
+	function getValue (fields, fieldName = undefined){
+		expect(data[field]).tobe('cats');
+	}
+	var fields ={};
+	function getValue (fields, fieldName = undefined){
+		expect(data[field]).not.tobe('');
+	}
+	var fields = {field: 'cats'};
+	function getValue (fields, fieldName = 'kitter'){
+		expect(data[fieldName]).tobe('cats');
+	}
+	var fields ={};
+	function getValue (fields, fieldName = 'kitter'){
+		expect(data[fieldName]).not.tobe('');
+	}
+	
 })
